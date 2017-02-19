0e139e377619801acc178285499ef84d56601d27
Olli Helenius <liff@iki.fi>
2017-02-19 16:36:22 +0200

    Ooops

--- dot.bash_functions.sh
+++ dot.bash_functions.sh
@@ -11,7 +11,7 @@ prepend_to_path_if_exists() {
 
     local dir
     for dir in "$@"; do
-        for entry in $PATH; do echo $entry $dir; [ "$entry" = "$dir" ] && continue 2; done
+        for entry in $PATH; do [ "$entry" = "$dir" ] && continue 2; done
         [ -d "$dir" ] && export PATH="${dir}:${PATH}"
     done
 }
